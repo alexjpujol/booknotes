19f227a071c3f15b1abddb23eb6add3c90323312
alexjpujol <alex.j.pujol@gmail.com>
2021-03-09 17:29:17 -0500

    Remove stubbed function

--- pages/api/books.ts
+++ pages/api/books.ts
@@ -1,12 +1,1 @@
-import { connectToDatabase } from "utils/mongodb";
-
-export default async (req, res) => {
-  const { db } = await connectToDatabase();
-  const books = await db
-    .collection("books")
-    .find({})
-    .sort({})
-    .limit(100)
-    .toArray();
-  res.status(200).json({ books });
-};
+export default async (req, res) => {};
